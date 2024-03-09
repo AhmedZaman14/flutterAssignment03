7a50990ed3d401ee75183c02f3156001b833f52c
Ahmed Zaman <zaman994ahmed@gmail.com>
2024-03-09 13:32:33 +0500

    Question 2

---
+++ Q2.dart
@@ -0,0 +1,12 @@
+void main() {
+  int age1 = 25;
+  int age2 = 30;
+
+  if (age1 > age2) {
+    print("First person is the oldest.");
+  } else if (age1 < age2) {
+    print("First person is the youngest.");
+  } else {
+    print("Both persons are of the same age.");
+  }
+}
